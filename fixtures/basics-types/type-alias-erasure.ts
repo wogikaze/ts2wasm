a48a4f6a60d3232fd34e681f0331022af6bf3880
wogikaze <kouse2319@gmail.com>
2026-04-28 23:17:04 +0900

    issue-059: erase TypeScript type aliases

---
+++ fixtures/basics-types/type-alias-erasure.ts
@@ -0,0 +1,20 @@
+type Id = number;
+
+export type Point = {
+  x: number;
+  y: number;
+  meta: { created: number };
+  translate: (dx: number, dy: number) => number;
+};
+
+function sum(point: Point): Id {
+  return point.x + point.y;
+}
+
+let origin: Point = {
+  x: 2,
+  y: 3,
+  meta: { created: 1 }
+};
+
+console.log(sum(origin));
