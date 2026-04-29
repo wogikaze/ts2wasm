3f8abf4ed9806baa119e4d3f55e1f2bb8309c019
wogikaze <kouse2319@gmail.com>
2026-04-29 11:00:36 +0900

    issue-052e: classify json stringify boxed args

--- fixtures/builtins-and-io/json-stringify-space-boxed-symbol.ts
+++ fixtures/builtins-and-io/json-stringify-space-boxed-symbol.ts
@@ -3,3 +3,8 @@ console.log(JSON.stringify({ a: 1 }, null, new String(">>")));
 console.log(JSON.stringify({ a: 1 }, null, Symbol));
 console.log(JSON.stringify({ a: 1 }, null, Symbol("gap")));
 console.log(JSON.stringify({ a: 1 }, null, new Boolean(true)));
+console.log(JSON.stringify({ a: 1 }, null, new Number(-2)));
+console.log(JSON.stringify({ a: 1 }, null, new Number()));
+console.log(JSON.stringify({ a: 1 }, null, new String()));
+console.log(JSON.stringify({ a: 1 }, null, new Boolean(false)));
+console.log(JSON.stringify({ a: 1 }, null, new Object()));
