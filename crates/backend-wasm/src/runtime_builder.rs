8b8e71bed792ea2e8560411b5f3c84a973e3762f
wogikaze <kouse2319@gmail.com>
2026-04-29 23:54:30 +0900

    issue-295: map split expression receiver

--- crates/backend-wasm/src/runtime_builder.rs
+++ crates/backend-wasm/src/runtime_builder.rs
@@ -107,6 +107,7 @@ impl WatEmitter<'_> {
                 RuntimeFn::ArrayPop => self.emit_array_pop(wat),
                 RuntimeFn::ArraySlice => self.emit_array_slice(wat),
                 RuntimeFn::ArrayConcat => self.emit_array_concat(wat),
+                RuntimeFn::ArrayMapStringSplit => self.emit_array_map_string_split(wat),
                 RuntimeFn::ArrayJoin => self.emit_array_join(wat),
                 RuntimeFn::ArrayReverse => self.emit_array_reverse(wat),
                 RuntimeFn::ObjectKeys => self.emit_object_keys(wat),
