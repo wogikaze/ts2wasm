cc878e66f4aa3f32b93840f41976e2359d918516
wogikaze <kouse2319@gmail.com>
2026-05-04 16:13:07 +0900

    fix: remove type annotations from array-concat fixture
    Type annotations (number[]) aren't supported in the test environment.

--- fixtures/builtins-and-io/array-concat.ts
+++ fixtures/builtins-and-io/array-concat.ts
@@ -1,6 +1,6 @@
 // Array.prototype.concat - flat shallow copy
-let a: number[] = [1, 2, 3];
-let b: number[] = [4, 5, 6];
+let a = [1, 2, 3];
+let b = [4, 5, 6];
 let c = a.concat(b);
 
 // Expected: [1, 2, 3, 4, 5, 6]
